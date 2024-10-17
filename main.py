20febc4849dd90eb404464252b697f8562d4fce3
MIKEZZZ-KING999 <micigwubor@gmail.com>
2024-10-17 17:57:09 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+import pgzrun
+from random import randint
+
+WIDTH = 600
+HEIGHT = 500
+
+score = 0
+game_over = False
+
+bee = Actor("bee")
+bee.pos = 100,100
+
+flower = Actor("flower")
+flower.pos = 200,200
+
+def draw():
+    screen.blit("background", (0,0))
+    flower.draw()
+    bee.draw()
+    screen.draw.text("Score: "+ str(score), color="black", topleft=(10,10))
+
+
+
+
+
+
+pgzrun.go()
